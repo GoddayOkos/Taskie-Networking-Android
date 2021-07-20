49df4e979ae66bd99e6425870706ae3094ce253f
Godday Okoduwa <goddayokos@gmail.com>
2021-07-20 20:57:51 +0100

    Added logging interceptor to retrofit

--- 02-set-up-the-project/starter/app/src/main/java/com/raywenderlich/android/taskie/networking/RetrofitBuilders.kt
+++ 02-set-up-the-project/starter/app/src/main/java/com/raywenderlich/android/taskie/networking/RetrofitBuilders.kt
@@ -1,12 +1,15 @@
 package com.raywenderlich.android.taskie.networking
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 
-fun buildClient(): OkHttpClient =
-    OkHttpClient.Builder()
+fun buildClient(): OkHttpClient = OkHttpClient.Builder()
+    .addInterceptor(HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    })
         .build()
 
 
